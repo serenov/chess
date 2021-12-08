0634abb2a1371e14cf6c005f9860a00d36d9f394
slayerof <73263527+serenov@users.noreply.github.com>
2021-12-08 11:10:30 +0530

    added display unit

--- display.c
+++ display.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
-void unit(short board[], short start, short status[]){
+void unit(short board[], short start, char status[]){
+    char decode(int value){
+        
+    }
     printf("----------------------------------------");
     printf("|%d%c ||%d%c ||%d%c ||%d%c ||%d%c ||%d%c ||%d%c ||%d%c ||", start, status[start], start + 1, status[start + 1], start + 2, status[start + 2], start + 3, status[start + 3], start + 4, status[start + 4], start + 5, status[start + 5], start + 6, status[start + 6], start + 7, status[start + 7], );
-    printf("|  %c ||  %c ||  %c ||  %c ||  %c ||  %c ||  %c ||  %c ||", board[start], board[start + 1], board[start + 2], board[start + 3], board[start + 4], board[start + 5], board[start + 6], board[start + 7]);
+    printf("|  %c ||  %c ||  %c ||  %c ||  %c ||  %c ||  %c ||  %c ||", decode(board[start]), decode(board[start + 1]), decode(board[start + 2]), decode(board[start + 3]), decode(board[start + 4]), decode(board[start + 5]), decode(board[start + 6]), decode(board[start + 7]));
     printf("----------------------------------------");
 }
