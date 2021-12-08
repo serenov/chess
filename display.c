24b1447c14c41be6685469b059a23c72771b5ed2
slayerof <73263527+serenov@users.noreply.github.com>
2021-12-08 11:24:22 +0530

    improved

--- display.c
+++ display.c
@@ -1,10 +1,15 @@
 #include<stdio.h>
 void unit(short board[], short start, char status[]){
     char decode(int value){
-        
+        char Wpieces[] = {' ','K', 'Q', 'R', 'B', 'N', 'P'}, Bpieces[] = {' ', 'k', 'q', 'r', 'b', 'n'};
+        if(value > 23) return ' ';
+        if(value < 16){
+            return Wpieces[value % 8]
+        }
+        return Bpieces[value % 8];
     }
-    printf("----------------------------------------");
-    printf("|%d%c ||%d%c ||%d%c ||%d%c ||%d%c ||%d%c ||%d%c ||%d%c ||", start, status[start], start + 1, status[start + 1], start + 2, status[start + 2], start + 3, status[start + 3], start + 4, status[start + 4], start + 5, status[start + 5], start + 6, status[start + 6], start + 7, status[start + 7], );
-    printf("|  %c ||  %c ||  %c ||  %c ||  %c ||  %c ||  %c ||  %c ||", decode(board[start]), decode(board[start + 1]), decode(board[start + 2]), decode(board[start + 3]), decode(board[start + 4]), decode(board[start + 5]), decode(board[start + 6]), decode(board[start + 7]));
-    printf("----------------------------------------");
+    printf("----------------------------------------\n");
+    printf("|%d%c ||%d%c ||%d%c ||%d%c ||%d%c ||%d%c ||%d%c ||%d%c ||\n", start, status[start], start + 1, status[start + 1], start + 2, status[start + 2], start + 3, status[start + 3], start + 4, status[start + 4], start + 5, status[start + 5], start + 6, status[start + 6], start + 7, status[start + 7], );
+    printf("|  %c ||  %c ||  %c ||  %c ||  %c ||  %c ||  %c ||  %c ||\n", decode(board[start]), decode(board[start + 1]), decode(board[start + 2]), decode(board[start + 3]), decode(board[start + 4]), decode(board[start + 5]), decode(board[start + 6]), decode(board[start + 7]));
+    printf("----------------------------------------\n");
 }
