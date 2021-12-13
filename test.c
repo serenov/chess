b749100f98f927e83d1b5147e83060c1e1396faf
bhanu <bhanuprakashrajubs@gmail.com>
2021-12-13 22:21:30 +0530

    updated

--- test.c
+++ test.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#define string "r3k2r/pppppppp/8/8/8/8/PPPPPPPP/R3K2R"
+#define string "r3k2r/pppppppp/5R2/8/8/8/PPPPP1PP/R3K2R"
 void display(short b[]);
 void fen(char *a, short b[], short *list);
 short linemove(short board[], short pos, short* possible);
@@ -44,9 +44,9 @@ short picker(short board[], short pos, short* possible){
 }
 void movmkr(short destpos, short board[], short pos, short* possible){
 	static short enpos = 0;
-	if(destpos == enpos){if(board[pos] > 16) board[destpos + 8] = none; else board[destpos - 8] = none;} 
-	if(board[enpos] > 22) board[enpos] = 0;
-	if((board[pos] % 8) == 6){ if(pos < 16 || pos > 47){if(destpos == (pos + 16)){ board[pos + 8] = 24; enpos = pos + 8; }else if(destpos == (pos - 16)){ board[pos - 8] = 25; enpos = pos - 8;}}}
+	if((board[pos] % 8) == 6){ if(pos < 16 || pos > 47){if(destpos == (pos + 16)){ board[pos + 8] = 24; enpos = pos + 8; }else if(destpos == (pos - 16)){ board[pos - 8] = 25; enpos = pos - 8;}}
+		if(destpos == enpos){if(board[pos] > 16) board[destpos + 8] = none; else board[destpos - 8] = none;} 
+	}
 	else if((board[pos] % 8) == 1){if(board[pos] > 16){if(!*(possible + 32)) *(possible + 32) = 1; kpos[1] = destpos;} else{ if(!*(possible + 29)) *(possible + 29) = 1; kpos[0] = destpos;}
 		if(destpos == pos - 2){board[pos - 1] = board[pos - 4]; board[pos - 4] = none;}		
 		if(destpos == pos + 2){board[pos + 1] = board[pos + 3]; board[pos + 3] = none;}		
@@ -61,6 +61,7 @@ void movmkr(short destpos, short board[], short pos, short* possible){
 			if(pos == 7) if(!*(possible + 30)) *(possible + 30) = 1;
 		}	
 	}
+	if(board[enpos] > 22) board[enpos] = 0;
 	board[destpos] = board[pos];
 	board[pos] = none;
 }
