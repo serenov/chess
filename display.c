310707ff4da1ad3a54ccc88f4f918fb98e166487
slayerof <73263527+serenov@users.noreply.github.com>
2021-12-13 11:46:31 +0530

    updated

--- display.c
+++ display.c
@@ -1,12 +1,12 @@
+char decode(int value){
+	char Wpieces[] = {' ','K', 'Q', 'R', 'B', 'N', 'P'}, Bpieces[] = {' ', 'k', 'q', 'r', 'b', 'n', 'p'};
+	if(value > 23) return ' ';
+	if(value < 16){
+		return Wpieces[value % 8];
+	}
+	return Bpieces[value % 8];
+}
 void unit(short board[], short start, char status[]){
-    char decode(int value){
-        char Wpieces[] = {' ','K', 'Q', 'R', 'B', 'N', 'P'}, Bpieces[] = {' ', 'k', 'q', 'r', 'b', 'n', 'p'};
-        if(value > 23) return ' ';
-        if(value < 16){
-            return Wpieces[value % 8];
-        }
-        return Bpieces[value % 8];
-    }
     printf("------------------------------------------------\n");
     printf("|%02d%c  |%02d%c  |%02d%c  |%02d%c  |%02d%c  |%02d%c  |%02d%c  |%02d%c  |\n", start, status[start], start + 1, status[start + 1], start + 2, status[start + 2], start + 3, status[start + 3], start + 4, status[start + 4], start + 5, status[start + 5], start + 6, status[start + 6], start + 7, status[start + 7]);
     printf("|  %c  |  %c  |  %c  |  %c  |  %c  |  %c  |  %c  |  %c  |\n", decode(board[start]), decode(board[start + 1]), decode(board[start + 2]), decode(board[start + 3]), decode(board[start + 4]), decode(board[start + 5]), decode(board[start + 6]), decode(board[start + 7]));
@@ -17,40 +17,4 @@ void disply(short b[], char status[]){
 	for(int i = 56; i >= 0; i = i - 8){
         unit(b, i, status);
 	}
-}	
-/*void fen(char *a, short bo[]){
-	for(int i = 56; i > -1;){
-		if(*a > 97){
-			if(*a == 'p') bo[i] = p;
-			else if(*a == 'n') bo[i] = n;
-			else if(*a == 'b') bo[i] = b;
-			else if(*a == 'r') bo[i] = r;
-			else if(*a == 'q') bo[i] = q;
-			else bo[i] = k;
-			i++;
-		}
-		else if(*a > 65){
-			if(*a == 'P') bo[i] = P;
-			else if(*a == 'N') bo[i] = N;
-			else if(*a == 'B') bo[i] = B;
-			else if(*a == 'R') bo[i] = R;
-			else if(*a == 'Q') bo[i] = Q;
-			else bo[i] = K;
-			i++;
-		}
-		else if(*a > 48){
-			for(int j = 1; j <= (*a - 48); j++){ bo[i] = none; i++;}
-		}
-		else i = i - 16;
-		a++;
-	}
 }
-int main(){
-    short board[64];
-    char status[64];
-    for(int i = 0; i < 64; i++) status[i] = ' ';
-    fen(string, board);
-    display(board, status);
-    return 0;
-}*/
-
--- input.c
+++ input.c
@@ -1,42 +1,13 @@
 #include<stdlib.h>
+char* t(short turn){ if(turn == 0) return "WHITE"; return "BLACK";}
+short d(short turn){if(turn == 0) return 8; return 16;}
 void game(){
 	short checkmate = 0, board[64], possible[34], mov = 0, turn;
 	char status[64];
 	for(short i = 0; i < 63; i++) {board[i] = none; status[i] = ' ';}
 	fen(string, board, possible + 28); 
-	char* t(short turn){ if(turn == 0) return "WHITE"; return "BLACK";}
-	short d(short turn){if(turn == 0) return 8; return 16;}
-	short decode(char c){ for(short i = 1; i <= possible[0]; i++) status[possible[i]] = c;}
-	short io(short turn){
-		short pos, destpos;
-		disply(board, status);
-		printf("PICK A PIECE: ");
-		scanf("%hd", &pos);
-		if((d(turn) & board[pos]) < 8){system("clear"); printf("INVALID SELECTION\n"); return 0;}
-		picker(board, pos, possible);
-		decode('*');
-		system("clear");
-		disply(board, status);
-		printf("PICK A MARKED SQUARE: ");
-		scanf("%hd", &destpos);
-		if(status[destpos] == '*')movmkr(destpos, board, pos, possible);
-		else{system("clear"); printf("INVALID SQUARE\n"); decode(' '); return 0;}
-		decode(' ');
-		system("clear");
-		printf("%d\n", board[22]);
-		if(kingthreat(board, kpos[!turn])){
-			printf("CHECK ON %s KING!", t(!turn));
-			for(short i = 0; i < 64; i++){
-				if((board[i] & d(!turn)) < 8);
-			       	else{
-					if(picker(board, i, possible)) return 1;
-				}
-			}
-			checkmate = 1;	
-		}
-		return 1;
-
-	}
+	short decode(char c);
+	short io(short turn);
 	system("clear");
 	do{
 		turn = mov % 2;
@@ -46,4 +17,34 @@ void game(){
 	disply(board, status);
 	printf("CHECKMATE! %s IS VICTORIOUS!\n", t(turn));
 }
+short io(short turn){
+	short pos, destpos;
+	disply(board, status);
+	printf("PICK A PIECE: ");
+	scanf("%hd", &pos);
+	if((d(turn) & board[pos]) < 8){system("clear"); printf("INVALID SELECTION\n"); return 0;}
+	picker(board, pos, possible);
+	decode('*');
+	system("clear");
+	disply(board, status);
+	printf("PICK A MARKED SQUARE: ");
+	scanf("%hd", &destpos);
+	if(status[destpos] == '*')movmkr(destpos, board, pos, possible);
+	else{system("clear"); printf("INVALID SQUARE\n"); decode(' '); return 0;}
+	decode(' ');
+	system("clear");
+	printf("%d\n", board[22]);
+	if(kingthreat(board, kpos[!turn])){
+		printf("CHECK ON %s KING!", t(!turn));
+		for(short i = 0; i < 64; i++){
+			if((board[i] & d(!turn)) < 8);
+				else{
+				if(picker(board, i, possible)) return 1;
+			}
+		}
+		checkmate = 1;	
+	}
+	return 1;
+}
+short decode(char c){ for(short i = 1; i <= possible[0]; i++) status[possible[i]] = c;}
 		
