667ad198b2954517ee91b602282e9efea0bed73d
slayerof <73263527+serenov@users.noreply.github.com>
2021-12-14 15:06:28 +0530

    added depth search

--- traverse.c
+++ traverse.c
@@ -1,13 +1,24 @@
-short search(){
+short turnd(short turn){
+	if(turn == 0) return 8;
+	return 16;
+}
+void search(short board[], short turn){
 	static short count = 0;
+	static short depth = 0;
+	if(depth == 8 || checkmate()) return;
+	int b[64];
+	for(short i = 0; i < 64; i++)b[i] = board[i];
 	for(short i = 0; i < 64; i++){
-		if(board[i] != none){
-			possible[0] = picker(baord, i, possible);
-			for(short j = 1; j <= possible[0]; j++){
-				movmkr(possible[j], board, i, possible);
-				board[i] = board[possible[j]];
-				board[possible[j]] = none;
-			}		
-		}	
+		if(board[i] != none)		
+			if(board[i] & turnd(turn) >= 8){
+				possible[0] = picker(baord, i, possible);
+				count += possible[0];
+				for(short j = 1; j <= possible[0]; j++){
+					movmkr(possible[j], board, i, possible);
+					search(b, !turn)
+					board[i] = board[possible[j]];
+					board[possible[j]] = none;
+				}		
+			}	
 	}
 }
