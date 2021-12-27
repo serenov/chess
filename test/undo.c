4781705bff72699a2a929b75667e2488123d2763
slayerof <73263527+serenov@users.noreply.github.com>
2021-12-27 12:02:50 +0530

    updated

--- test/undo.c
+++ test/undo.c
@@ -3,6 +3,12 @@ void revertEn(short *board, short *list){
         if(*(list + 2) > 0) capturedpiece = P;
         else capturedpiece = P;
         *(board + *(list + 1) + *(list + 2)) = capturedpiece;
+        *(board + *list) = *(board + *(list + 1));
+        *(board + *(list + 1)) = none;
+}
+void revertR(short *board, short *list){
+        *(board + *list) = *(board + *(list + 1));
+        *(board + *(list + 1)) = *(board + *(list + 2));
 }
 void undo(short *board, short *list, short flag){
 
