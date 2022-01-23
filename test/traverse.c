a2fe8c72c63542dafd8f9efb1d22b9494bc8bb43
slayerof <73263527+serenov@users.noreply.github.com>
2022-01-23 19:48:53 +0530

    fixed problems

--- test/traverse.c
+++ test/traverse.c
@@ -1,19 +1,23 @@
 #include"../include.h"
 #include"undo.c"
 short turnd(short turn){ if(turn == 0) return 8; return 16;}
-short offsetd(short turn){ if(turn == 0) return 8; return -8;}
+short offsetd(short turn, short flag){
+        if(flag == 0){if(turn == 0) return 8; return 16;} else if(flag == 1){ if(turn == 0) return 9; return -9;}
+        else{ if(turn == 0) return 7; return -7;}
+}
 short checkmate(short board[], short color, short *possible){
-	for(short i = 0; i < 64; i++){
-		if((board[i] & color) >= 8){
-			if(picker(board, i, possible)) return 0;
-		}
-	}
-	return 1;
+        for(short i = 0; i < 64; i++){
+                if((board[i] & color) >= 8){
+                        if(picker(board, i, possible)) return 0;
+                }
+        }
+        return 1;
 }
 void init(short *kp){
         *kp = kpos[0];
         *(kp + 1) = kpos[1];
-}void dinit(short *kp){
+}
+void dinit(short *kp){
         kpos[0] = *kp;
         kpos[1] = *(kp + 1);
 }
@@ -29,17 +33,17 @@ void FDecide(short board[], short *flag, short i, short tpossible, short list[],
 int count = 0;
 char status[64];
 void traverse(short board[], short *possible, short turn, short depth){
-	short kp[2];
+        short kp[2];
         short tpossible[35], capturedpiece = none, flag, list[3];
-        init(kp); 
-	if(kingthreat(board, kp[turn])){if(checkmate(board, turnd(turn), tpossible)) return;}
-        if(depth > 4) return;
+        init(kp);
+        if(kingthreat(board, kp[turn])){if(checkmate(board, turnd(turn), tpossible))return;}
+        if(depth > 3) return;
         for(short i = 0; i < 7; i++){ tpossible[i + 28] = *(possible + i);}
         for(short i = 0; i < 64; i++){
                 if(board[i] != none){
                         if((board[i] & turnd(turn)) >= 8){
                                 tpossible[0] = picker(board, i, tpossible);
-                                if(depth == 0)count += tpossible[0];
+                                if(depth == 3)count += tpossible[0];
                                 for(short j = 1; j <= tpossible[0]; j++){
                                         FDecide(board, &flag, i, tpossible[j], list, turn);
                                         movmkr(tpossible[j], board, i, tpossible);
@@ -47,8 +51,8 @@ void traverse(short board[], short *possible, short turn, short depth){
                                         undo(board, list, flag);
                                         dinit(kp);
                                         for(short i = 0; i < 7; i++){ tpossible[i + 28] = *(possible + i);}
-				}
-			}
+                                }
+                        }
                 }
         }
 }
@@ -56,9 +60,9 @@ int main(){
         short board[64], possible[35];
         for(short i = 0; i < 64; i++){board[i] = none; status[i] = ' ';}
         for(short i = 28; i < 34; i++) possible[i] = 1;
-	possible[34] = -1;
+        possible[34] = -1;
         fen(string, board, possible + 28);
         traverse(board, possible + 28, 0, 0);
-        printf("%d\n", count);
+        printf("count: %d\n", count);
         return 0;
 }
