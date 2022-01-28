17decefab18dc9e24d7b0a6f4cdd0da1db5976cb
bhanu <bhanuprakashrajubs@gmail.com>
2022-01-28 23:39:07 +0530

    made unmessy

--- dynamics/stalemate.c
+++ dynamics/stalemate.c
@@ -2,7 +2,7 @@ short stalemate(short board[], short turn){
 	short possible[34], color = (turn == 0)? 8: 16;
 	for(short i = 0; i < 64; i++){
 		if((board[i] & color) >= 8){
-			if(possible[0] = picker(board, i, possible) > 0) return 0;
+			if((possible[0] = picker(board, i, possible)) > 0) return 0;
 		}
 	}	
 	return 1;
